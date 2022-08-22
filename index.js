476365a0574c46379c44f68beace2c44003bc438
dennis00095 <dennis.mwongela@student.moringaschool.com>
2022-08-22 14:51:43 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+let multiply = (num1 * num2)
+let num1 = 31
+let num2 = 2
+multiply = 62;
+
+const random = 1
+
+const mod = 4
+let num3 = 40
+let num4 = 10
+num3 % num4;
+
+const max = Math.max(20);
